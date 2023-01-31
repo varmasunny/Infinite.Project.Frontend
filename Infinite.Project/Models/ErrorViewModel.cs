a56cae7b0287ae5f6015a4e06f5f5b47bf552cc9
narendravarmak <narendravarmak@ICS-LAP-2689>
2023-01-31 11:55:27 +0530

    Add project files.

---
+++ Infinite.Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Infinite.Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Infinite.Project/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
